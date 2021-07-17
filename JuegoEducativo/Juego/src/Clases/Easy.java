731187af1852365d40d11c1b6e7370b85f474a5c
ArmandoAguilar11 <67571468+ArmandoAguilar11@users.noreply.github.com>
2021-07-16 19:22:19 -0600

    clases facil

---
+++ JuegoEducativo/Juego/src/Clases/Easy.java
@@ -0,0 +1,70 @@
+
+package Clases;
+
+
+public class Easy {
+    private String pregunta;
+    private String correcta;
+    private String A;
+    private String B;
+    private String C;
+    private String D;
+    
+    public Easy(){
+        pregunta = "";
+        correcta = "";
+        A = "";
+        B = "";
+        C = "";
+        D = "";
+    }
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getCorrecta() {
+        return correcta;
+    }
+
+    public void setCorrecta(String correcta) {
+        this.correcta = correcta;
+    }
+
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String A) {
+        this.A = A;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String B) {
+        this.B = B;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String C) {
+        this.C = C;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String D) {
+        this.D = D;
+    }
+    
+}
---
+++ JuegoEducativo/Juego/src/Clases/EasyList.java
@@ -0,0 +1,7 @@
+package Clases;
+
+import java.util.ArrayList;
+
+public class EasyList {
+    ArrayList<Easy> lista = new ArrayList();
+}
